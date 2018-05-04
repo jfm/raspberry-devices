38823c4c5b77c6ab48c67daa0044113663f1ea97
jfm <jfm@moerks.dk>
2018-05-04 13:59:29 +0200

    Starting work on motor device and corresponding GPIO library

---
+++ RPi/GPIO.py
@@ -0,0 +1,25 @@
+LOW = 0
+HIGH = 1
+
+BOARD = 10
+BCM = 11
+
+
+def setmode(mode):
+    pass
+
+
+def setup():
+    pass
+
+
+def input(channel):
+    pass
+
+
+def output(channel, outmode):
+    pass
+
+
+def cleanup(channel=None):
+    pass
